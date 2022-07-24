2523e9820a972d19f7775772a9f739bc089aa447
shishc9 <52554717+shishc9@users.noreply.github.com>
2022-07-25 00:07:28 +0800

    feat: add watcher

--- zookeeper/src/main/java/icu/shishc/ZkApplication.java
+++ zookeeper/src/main/java/icu/shishc/ZkApplication.java
@@ -1,13 +1,25 @@
 package icu.shishc;
 
+import icu.shishc.consts.ZkConstants;
+import icu.shishc.zookeeper.ZkConnection;
+import org.apache.zookeeper.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class ZkApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
         SpringApplication.run(ZkApplication.class, args);
+
+        ZkConnection connection = new ZkConnection();
+        ZooKeeper zooKeeper = connection.connect();
+        // 注册 -> 存储 -> 通知 -> 回调. 当/${zkPath}/create_node/* 任意子目录发生变化，则会回调watcher.
+        zooKeeper.addWatch("/" + ZkConstants.zkPath + "/create_node", event -> {
+            System.out.println("something happen");
+        }, AddWatchMode.PERSISTENT_RECURSIVE);
     }
 
 }
